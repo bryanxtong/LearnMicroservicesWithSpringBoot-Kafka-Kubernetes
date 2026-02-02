21b02d621d8240c18ce8247460ca63fdb576249c
bryanxtong <bryanxtong@gmail.com>
2026-02-02 12:21:30 +0800

    update to spring cloud kubernetes 2025.1.0 and spring boot 4.0.1

--- challenges-frontend/src/services/GameApiClient.js
+++ challenges-frontend/src/services/GameApiClient.js
@@ -1,5 +1,5 @@
 class GameApiClient {
-    static SERVER_URL = 'http://localhost:8000';
+    static SERVER_URL =  process.env.REACT_APP_API_URL || '/api';
     static GET_LEADERBOARD = '/leaders';
 
     static leaderBoard(): Promise<Response> {
